11a585192e6f2fd72033a0d69fc929b65207f58c
Кирилл <prihodkovkirill@Gmail.com>
2025-12-24 17:27:34 +0700

    add first model: Document

---
+++ MaxWord/Models/Document.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxWord.Models
+{
+    enum Formates
+    {
+        txt = 0,
+        pdf = 1,
+        rtf = 2
+    }
+    internal class Document
+    {
+        private string _text;
+
+        public string Text
+        {
+            get { return _text; }
+            set 
+            { 
+                _text = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Formates Form { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
