2ae9c9adf4463ce0865d1d4cef3aee2a775237a1
дубащитбл <tbahilin@gmail.com>
2025-12-25 16:16:58 +0700

    add MainWindow.xaml.cs

---
+++ MaxWord/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MaxWord
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NewFile_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveFile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Copy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Paste_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Bold_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Italic_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Underline_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FontFamilyChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void FontSizeChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Editor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
